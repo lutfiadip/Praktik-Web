ac2b8d1e93d8c24d9c9af61033688685f26357c9
Lutfi Adi P <lutfiadiprakoso@students.unnes.ac.id>
2025-10-23 18:23:55 +0700

    ganti username dan password

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$username_valid = "unnes";
-$password_valid = "12345";
+$username_valid = "blackpink";
+$password_valid = "blink";
 
 if(!isset($_POST['username']) || !isset($_POST['password'])) {
     header("Location: index.html");
@@ -13,14 +13,12 @@ $password = $_POST["password"];
 
 if (($username == $username_valid) && ($password == $password_valid)) {
     
-
     $_SESSION["login"][]=[
         "username" => $username,
         "password" => $password,
         "login_at" => date('Y-m-d H:i:s')
     ];
 
-    
     echo "<h2>Selamat Datang!!!</h2>". $username.",anda sudah login sebanyak: ".count($_SESSION["login"])." kali";
 
     echo '<br>';
@@ -28,9 +26,27 @@ if (($username == $username_valid) && ($password == $password_valid)) {
     echo "<a href='logout.php'>Logout</a>";
     
     echo '<pre>';
-    var_dump($_SESSION['login']);
-    echo '</pre>';
+    //var_dump($_SESSION['login']);
 
+?>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>Username</th>
+            <th>Password</th>
+            <th>Login At</th>
+        </tr>
+        <?php foreach($_SESSION['login'] as $index => $login) : ?>
+        <tr>
+            <td><?= $index + 1; ?></td>
+            <td><?= $login['username']; ?></td>
+            <td><?= $login['password']; ?></td>
+            <td><?= $login['login_at']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </table>
+    </pre>
+<?php
 } else {
     echo "<h2>Username atau password salah</h2>";
 }
